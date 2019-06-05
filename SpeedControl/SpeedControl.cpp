c4ece6f66639d65bcb25c524c64c99ce0b3eccc9
lekoook <kokteng1313@gmail.com>
2019-06-05 16:42:32 +0800

    Improve SpeedControl to work with dual motors
    SpeedControl class can now be used to work with 2 motors. Separate
    functions can be called for each motor. See code comments.

--- SpeedControl/SpeedControl.cpp
+++ SpeedControl/SpeedControl.cpp
@@ -3,9 +3,9 @@
 /**
  * This class allows PID speed (degrees/second) control of motors.
  * 
- * Each instance requires a Motor and an Encoder object to work. This system reads speed from the Encoder object and 
- * calculates the error between the set point (desired speed, set using setSpeed()) and process variable (current 
- * measured speed).
+ * Each instance requires a Motor and two Encoder objects (one for each motor) to work. This system reads speed from 
+ * the Encoder object and calculates the error between the set point (desired speed, set using setSpeed()) and process 
+ * variable (current measured speed).
  * 
  * This error is then fed into the PID equation and it outputs the PWM adjustment value. This PWM value is then used 
  * to set the motor's PWM using Motor object.
@@ -14,11 +14,10 @@
  * where a new PWM adjustment value is calculated and used to set motor PWM. This loop process continues until the set 
  * point is equal to process variable (desire speed is met).
  * 
- * NOTE: Depending on the Arduino you are using (Uno, Due, Zero, etc.), int can be stored as 2-bytes or 4-bytes values.
+ * NOTE: Depending on the Arduino you are using (Uno, Due, Zero, etc.), int can be stored as 2-bytes or 4-bytes values. 
  * In certain cases, you may wish to replace long with int to save memory. Correspondingly, be wary with the fact that
  * int are stored as 2-bytes on some boards (eg. Uno). You may then want to consider overflow cases. 
  * 
- * TODO: Accomodate for dual motors since Motor class controls dual motors.
  */
 
 /**
@@ -27,76 +26,139 @@
  * @param motor Motor object used.
  * @param encoder Encoder object used.
  */
-SpeedControl::SpeedControl(Motor *motor, Encoder *encoder) : motor(motor), encoder(encoder) {
-  pwm = 0;
-  setPoint = 0;
-  prevSpeed = 0;
+SpeedControl::SpeedControl(Motor *motor, Encoder *encoderOne, Encoder *encoderTwo) 
+            : motor(motor), 
+            encoderOne(encoderOne), 
+            encoderTwo(encoderTwo) 
+  {
+  pwmM1 = 0;
+  pwmM2 = 0;
+  setPointM1 = 0;
+  setPointM2 = 0;
+  prevM1Speed = 0;
+  prevM2Speed = 0;
   minSpeed = DEF_MIN_SPEED;
-  pGain = DEF_GAIN;
-  iGain = DEF_GAIN;
-  dGain = DEF_GAIN;
-  iTerm = 0;
+  pGainM1 = DEF_GAIN;
+  iGainM1 = DEF_GAIN;
+  dGainM1 = DEF_GAIN;
+  iTermM1 = 0;
+  pGainM2 = DEF_GAIN;
+  iGainM2 = DEF_GAIN;
+  dGainM2 = DEF_GAIN;
+  iTermM2 = 0;
 };
 
 /**
- * @brief Corrects the PWM of the motor to maintain the speed of set point.
+ * @brief Corrects the PWM of the first motor to maintain the speed of set point.
  * @details This function must be called every deltaTime (in Encoder class).
  * 
  */
-void SpeedControl::correctPwm(void)
+void SpeedControl::correctM1Pwm(void)
 {
   // Get the scalar speed.
-  long speed = encoder->getSpeed();
+  long speed = encoderOne->getSpeed();
   if (speed < 0)
   {
     speed *= -1;
   }
 
   // Find the error.
-  long error = setPoint - speed;
+  long error = setPointM1 - speed;
 
   // Calculate P, I, D values.
-  double pTerm = pGain * (double)error;
-  iTerm += iGain * (double)error;
-  double dTerm = dGain * (speed - prevSpeed); // Uses derivative on measurement. See: http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-derivative-kick/
+  double pTerm = pGainM1 * (double)error;
+  iTermM1 += iGainM1 * (double)error;
+  double dTerm = dGainM1 * (speed - prevM1Speed); // Uses derivative on measurement. See: http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-derivative-kick/
 
   // Calculate the pwm value to set.
-  int correction = pTerm + iTerm - dTerm;
-  pwm += correction;
-  if (pwm > 255)
+  int correction = pTerm + iTermM1 - dTerm;
+  pwmM1 += correction;
+  if (pwmM1 > 255)
   {
-    pwm = 255;
+    pwmM1 = 255;
   }
-  if (pwm < 0)
+  if (pwmM1 < 0)
   {
-    pwm = 0;
+    pwmM1 = 0;
   }
    
   // Set pwm.
-  motor->setM1Pwm(pwm);
-  prevSpeed = speed;
+  motor->setM1Pwm(pwmM1);
+  prevM1Speed = speed;
 }
 
 /**
- * @brief Sets the P, I, D gains.
+ * @brief Corrects the PWM of the second motor to maintain the speed of set point.
+ * @details This function must be called every deltaTime (in Encoder class).
  * 
- * @param pGain Proportional gain.
- * @param iGain Integral gain.
- * @param dGain Derivative gain.
  */
-void SpeedControl::setGains(double pGain, double iGain, double dGain)
+void SpeedControl::correctM2Pwm(void)
 {
-  this->pGain = pGain;
-  this->iGain = iGain;
-  this->dGain = dGain;
+  // Get the scalar speed.
+  long speed = encoderTwo->getSpeed();
+  if (speed < 0)
+  {
+    speed *= -1;
+  }
+
+  // Find the error.
+  long error = setPointM2 - speed;
+
+  // Calculate P, I, D values.
+  double pTerm = pGainM2 * (double)error;
+  iTermM2 += iGainM2 * (double)error;
+  double dTerm = dGainM2 * (speed - prevM2Speed); // Uses derivative on measurement. See: http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-derivative-kick/
+
+  // Calculate the pwm value to set.
+  int correction = pTerm + iTermM2 - dTerm;
+  pwmM2 += correction;
+  if (pwmM2 > 255)
+  {
+    pwmM2 = 255;
+  }
+  if (pwmM2 < 0)
+  {
+    pwmM2 = 0;
+  }
+   
+  // Set pwm.
+  motor->setM2Pwm(pwmM2);
+  prevM2Speed = speed;
 }
 
 /**
- * @brief Sets the set point speed (in degrees/second).
+ * @brief Sets the P, I, D gains of both motors.
+ * 
+ * @param pGainM1 Proportional gain of first motor.
+ * @param iGainM1 Integral gain of first motor.
+ * @param dGainM1 Derivative gain first motor.
+ * @param pGainM2 Proportional gain of second motor.
+ * @param iGainM2 Integral gain of second motor.
+ * @param dGainM2 Derivative gain second motor.
+ */
+void SpeedControl::setGains(
+      double pGainM1, 
+      double iGainM1, 
+      double dGainM1, 
+      double pGainM2, 
+      double iGainM2, 
+      double dGainM2
+      )
+{
+  this->pGainM1 = pGainM1;
+  this->iGainM1 = iGainM1;
+  this->dGainM1 = dGainM1;
+  this->pGainM2 = pGainM2;
+  this->iGainM2 = iGainM2;
+  this->dGainM2 = dGainM2;
+}
+
+/**
+ * @brief Sets the set point speed (in degrees/second) of first motor.
  * 
  * @param speed Value to set the set point with.
  */
-void SpeedControl::setSpeed(long speed)
+void SpeedControl::setM1Speed(long speed)
 {
   if (speed < 0)
   {
@@ -117,7 +179,36 @@ void SpeedControl::setSpeed(long speed)
     speed = minSpeed;
   }
 
-  setPoint = speed;
+  setPointM1 = speed;
+}
+
+/**
+ * @brief Sets the set point speed (in degrees/second) of second motor.
+ * 
+ * @param speed Value to set the set point with.
+ */
+void SpeedControl::setM2Speed(long speed)
+{
+  if (speed < 0)
+  {
+    motor->setM2Dir(DIR_BWD);
+    speed *= -1;
+  }
+  else if (speed > 0)
+  {
+    motor->setM2Dir(DIR_FWD);
+  }
+  else
+  {
+    motor->stopM2();
+    motor->setM2Pwm(0);
+  }
+
+  if (speed > 0 && speed < minSpeed){
+    speed = minSpeed;
+  }
+
+  setPointM2 = speed;
 }
 
 /**
--- SpeedControl/SpeedControl.h
+++ SpeedControl/SpeedControl.h
@@ -7,24 +7,41 @@
 class SpeedControl
 {
   public:
-    SpeedControl(Motor *motor, Encoder *encoder);
-    void correctPwm(void);
-    void setGains(double pGain, double iGain, double dGain);
-    void setSpeed(long speed);
+    SpeedControl(Motor *motor, Encoder *encoderOne, Encoder *encoderTwo);
+    void correctM1Pwm(void);
+    void correctM2Pwm(void);
+    void setGains(
+      double pGainM1, 
+      double iGainM1, 
+      double dGainM1, 
+      double pGainM2, 
+      double iGainM2, 
+      double dGainM2
+      );
+    void setM1Speed(long speed);
+    void setM2Speed(long speed);
     void setMinSpeed(long speed);
 
   private:
     Motor *motor;
-    Encoder *encoder;
+    Encoder *encoderOne;
+    Encoder *encoderTwo;
 
-    int pwm;
+    int pwmM1;
+    int pwmM2;
 
-    long setPoint; // Desired speed in degrees/second.
-    long prevSpeed; // Previous speed set.
+    long setPointM1; // Desired speed in degrees/second.
+    long setPointM2; // Desired speed in degrees/second.
+    long prevM1Speed; // Previous speed set.
+    long prevM2Speed; // Previous speed set.
     long minSpeed; // Minimum speed.
 
-    double pGain;
-    double iGain;
-    double dGain;
-    double iTerm; // Keep track of integral term.
+    double pGainM1;
+    double iGainM1;
+    double dGainM1;
+    double iTermM1; // Keep track of integral term.
+    double pGainM2;
+    double iGainM2;
+    double dGainM2;
+    double iTermM2; // Keep track of integral term.
 };
